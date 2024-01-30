215f060299cb5702f1b06d4db7025560ae79836a
Amina Azaiez <amina.azaiez.aa@gmail.com>
2024-01-30 14:54:09 +0100

    stepwise selection ols + core_decomposition

---
+++ Programs/utils/core.py
@@ -0,0 +1,171 @@
+import xgi
+import hypernetx as hnx
+import numpy as np
+import itertools
+
+
+def build_hypergraph(H: hnx.Hypergraph):
+    data = [H.edges[e] for e in H.edges]
+    X = xgi.Hypergraph(data) #build the hypergraph
+    X.merge_duplicate_edges()
+
+    return(X)
+
+def restrict_m_hypergraph(X : xgi.Hypergraph, m: int , connected =False, intimate = False):
+    ''' m : minimum size of hyperedge'''
+    if intimate :
+        e_remove = [edge for edge in X.edges if X.edges.size[edge] > m]
+    else :
+        e_remove = [edge for edge in X.edges if X.edges.size[edge] < m]
+
+    X.remove_edges_from(e_remove)
+    X.merge_duplicate_edges()
+    still_2_remove = len(e_remove) > 0
+
+    if connected :
+        try:
+            X.cleanup(isolates=False, singletons=False, multiedges=False, connected=True, relabel=False, in_place=True) # keep the larget connected component
+        except:
+            pass
+    return(X, still_2_remove)
+
+def restrict_k_hypergraph(X : xgi.Hypergraph, k: int, connected = False ):
+    ''' k : minimum degree of nodes'''
+    n_remove = [node for node in X.degree().keys() if X.degree()[node]< k]
+    X.remove_nodes_from(n_remove)
+    X.merge_duplicate_edges()
+    still_2_remove = len(n_remove) > 0
+
+    if connected :
+        try:
+            X.cleanup(isolates=False, singletons=False, multiedges=False, connected=True, relabel=False, in_place=True) # keep the larget connected component
+        except:
+            pass
+    return(X , still_2_remove)
+
+
+
+def core_decomposition(H : hnx.Hypergraph, connected = False, intimate = False) :
+    X = build_hypergraph(H)
+    if intimate :
+        M = range( max(X.edges.size.asnumpy()) + 1 , 1 , -1) # m = edge size
+    else:
+        M = range(2 , max(X.edges.size.asnumpy()) + 1 ) # m = edge size
+
+
+    K = range(max(X.degree().values()))
+    core = { m : {} for m in M } # m-shell index
+
+    for m in M: # loop for the m shells
+        # For each m, start with the initial hypergraph restricted to the edges of size >= m
+
+        k = 1
+        X = build_hypergraph(H)
+        while X.num_nodes > 0 : # loop for the k,m shell
+            print(m,k)
+
+            X , still_nodes_2_remove = restrict_m_hypergraph(X, m, connected, intimate)
+            X , still_edges_2_remove = restrict_k_hypergraph(X, k, connected)
+            # Store previous shell to compute the k,m shell at the end of the loop
+
+            while  still_nodes_2_remove or still_edges_2_remove : # redo untill there are nither nodes nore edges that can be removed
+
+                X , still_nodes_2_remove = restrict_m_hypergraph(X, m, connected, intimate)
+                X , still_edges_2_remove = restrict_k_hypergraph(X, k, connected)
+
+            if X.num_nodes > 0 :
+                core[m][k] = [list(component) for component in xgi.connected_components(X)]
+
+                k += 1
+
+    return(core)
+
+def k_m_core(H : hnx.Hypergraph, k, m):
+
+    X = build_hypergraph(H)
+
+    X , still_nodes_2_remove = restrict_m_hypergraph(X, m, )
+    X , still_edges_2_remove = restrict_k_hypergraph(X, k, )
+
+    while  still_nodes_2_remove or still_edges_2_remove : # redo untill there are nither nodes nore edges that can be removed
+
+            X , still_nodes_2_remove = restrict_m_hypergraph(X, m, )
+            X , still_edges_2_remove = restrict_k_hypergraph(X, k, )
+    return(X)
+
+
+
+
+
+def k_m_largest_cc_size(core,m , k):
+    components = core[m][k]
+    if len(components)>1:
+        components.sort(key = len, reverse = True)
+        return(len(components[0]) /( len(components[0])+ len(components[1]) ) )
+    else :
+        return(1)
+
+def proportion_largest_cc_size(core):
+    K = set(list (itertools.chain.from_iterable([ core[i].keys() for i in core.keys()])))
+    M = core.keys()
+    array = np.zeros( (len(M),len(K) ))
+    for i,m in enumerate( M):
+        for j,k in enumerate(K):
+            try:
+                array[i,j] = k_m_largest_cc_size(core, m,k)
+            except :
+                pass
+    return(array)
+
+
+def k_m_core_size(core):
+    K = set(list (itertools.chain.from_iterable([ core[i].keys() for i in core.keys()])))
+    M = core.keys()
+
+    n_k_m =  np.zeros( (len(M),len(K) ))
+    for i,m in enumerate( M):
+        for j,k in enumerate(K):
+            try:
+                n_k_m[i,j] = sum([len(component) for component in core[m][k]])
+            except :
+                pass
+    return(n_k_m)
+
+def species_survival(core, m , k , node_set):
+    k_m_core = core[m][k]
+    nodes = set()
+    for component in k_m_core:
+        nodes = nodes.union(component)
+
+    return( len( nodes & node_set ) )
+
+
+
+def reverse_y_axis( A ):
+    Y = np.shape(A)[0]
+    X = np.shape(A)[1]
+
+    array = np.zeros_like(A)
+    for y in range(Y):
+        for x in range(X):
+            array [ Y - y-1,x]=  A[ y, x]
+    return(array)
+
+
+def exiting_shell(core) :
+    x_min = min(core.keys())
+    y_min = min(core[x_min].keys())
+    c_m = { node : {} for node in core[x_min][y_min][0]}
+    for x in core.keys():
+        y_max = max(core[x].keys())
+        for y in core[x].keys():
+            if y != y_max :
+                x_y_shell = core[x][y][0] - core[x][y+1][0]
+            else :
+                x_y_shell = core[x][y][0]
+
+            for node in x_y_shell:
+                c_m[node][x] = y
+    return(c_m)
+
+
--- Programs/utils/ols.py
+++ Programs/utils/ols.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import statsmodels.api as sm
+from sklearn import preprocessing
 from scipy.stats import yeojohnson, skew
 from scipy import stats
 import seaborn as sns
@@ -25,6 +26,23 @@ def get_skewed_columns(df):
                  .query('abs(Skew) > {}'.format(skew_limit)))
     return skew_cols
 
+def get_similar_value_cols(df, percent=90):
+    """
+    :param df: input data in the form of a dataframe
+    :param percent: integer value for the threshold for finding similar values in columns
+    :return: sim_val_cols: list of columns where a singular value occurs more than the threshold
+    """
+    count = 0
+    sim_val_cols = []
+    for col in df.columns:
+        percent_vals = (df[col].value_counts()/len(df)*100).values
+        # filter columns where more than 90% values are same and leave out binary encoded columns
+        if percent_vals[0] > percent :
+            sim_val_cols.append(col)
+            count += 1
+    print("Total columns with majority singular value shares: ", count)
+    return sim_val_cols
+
 
 def one_hot_encod(df : pd.DataFrame , col : list , to_drop = None):
     """
@@ -32,11 +50,15 @@ def one_hot_encod(df : pd.DataFrame , col : list , to_drop = None):
     :param columns: list of columns to encode
     :return: df: dataframe with the encoded columns
     """
-    if to_drop is not None:
+
+    if to_drop == 'first':
+        dummies  =  pd.get_dummies(df[col], drop_first=True, prefix_sep = '', prefix = '', dtype = int)
+    elif to_drop is not None:
         dummies  =  pd.get_dummies(df[col],  prefix_sep = '', prefix = '', dtype = int)
         dummies.drop(columns = to_drop, inplace = True)
+
     else :
-        dummies  =  pd.get_dummies(df[col], drop_first=True, prefix_sep = '', prefix = '', dtype = int)
+        dummies  =  pd.get_dummies(df[col], prefix_sep = '', prefix = '', dtype = int)
 
     df = pd.concat([df.drop(col, axis=1), dummies], axis=1)
     return (df , list(dummies.columns))
@@ -48,8 +70,8 @@ def remove_outliers(df, col :str, threshold):
 
     cat_to_remove = list( df_[df_<threshold].index)
     com_to_remove = list(df[df[col].isin(cat_to_remove)].index)
-    #print('removed category: ' , cat_to_remove)
-    #print('number of outliers: ', len(com_to_remove))
+    print('removed category: ' , cat_to_remove)
+    print('number of outliers: ', len(com_to_remove))
 
     return(df[~df[col].isin(cat_to_remove)])
 
@@ -84,90 +106,331 @@ def xy_heatmap_corr (df , col1, col2, **kwargs ):
     plt.show()
 
 
+data  = EU.get_companies()[[ 'Degree', 'Strength', 'Betweenness','EV_linear', 'EV_log exp', 'EV_max', 'Hypercoreness', 'TR Country','Members FTE',  'Assets','Fields of interest', 'Level of interest', 'NACE']].dropna().copy()
 
-data  = pd.read_csv(path + 'Programs/data_test_R.csv')
+#Split catagorial data
 
-dependent = ['Strength', 'Degree', 'Betweenness', 'Closeness']
+levels = split_column(data, 'Level of interest', prefix = 'Level')
+data.drop('Level of interest', axis = 1, inplace = True)
+fields  = split_column(data , 'Fields of interest', prefix = 'Field')
+data.drop('Fields of interest', axis = 1, inplace = True)
+
+data = pd.concat( [data , levels, fields], axis = 1)
+
+
+dependent = ['Degree', 'Strength', 'Betweenness', 'EV_linear', 'EV_log exp', 'EV_max', 'Hypercoreness']
 
 lobbying = ['Members FTE']
-financials = ['Revenue', 'Nb employees', 'Assets']
+financials = ['Assets']
+#, 'Nb employees', 'Revenue']
 
 country = ['TR Country']
 
-sector = ['BvD sectors']
+sector = ['NACE']
 
-level = ['Level_Global', 'Level_Regional/local', 'Level_National',
-       'Level_European']
-field = ['Field_Economy', 'Field_Enlargement',
-       'Field_Food safety', 'Field_Trans-European Networks',
-       'Field_Business and industry',
-       'Field_Agriculture and rural development', 'Field_Youth',
-       'Field_Migration and asylum', 'Field_Climate action', 'Field_Trade',
-       'Field_Sport', 'Field_Borders and security', 'Field_Culture and media',
-       'Field_Humanitarian aid and civil protection', 'Field_Single market',
-       'Field_finance and the euro', 'Field_Education and training',
-       'Field_Fraud prevention', 'Field_Transport', 'Field_Taxation',
-       'Field_Institutional affairs',
-       'Field_International co-operation and development', 'Field_Customs',
-       'Field_External relations', 'Field_Banking and financial services',
-       'Field_Maritime affairs and fisheries', 'Field_Competition',
-       'Field_Energy', 'Field_Public health', 'Field_Environment',
-       'Field_Justice and fundamental rights', 'Field_Culture',
-       'Field_Consumers', 'Field_Regional policy',
-       'Field_Research and innovation', 'Field_Digital economy and society',
-       'Field_Communication', 'Field_European neighbourhood policy',
-       'Field_Budget', 'Field_Employment and social affairs',
-       'Field_Foreign affairs and security policy']
-
+level = list(levels.columns)
+field = list(fields.columns)
+# Group countries
 
 country_groups = {
     'North America': ['CANADA', 'UNITED STATES'],
+    'Central and South America': ['MEXICO', 'BRAZIL'],
     'North Europe': ['NORWAY', 'FINLAND', 'DENMARK', 'SWEDEN',  'LITHUANIA', 'LATVIA', 'ESTONIA', 'UNITED KINGDOM', 'IRELAND'],
-    'South Europe': ['ITALY', 'SPAIN', 'PORTUGAL', 'GREECE', 'CROATIA', 'CYPRUS'],
-    'East Europe': ['CZECH REPUBLIC', 'POLAND',   'ROMANIA', 'HUNGARY', 'BULGARIA', 'SLOVAKIA', 'KOSOVO'],
+    'South Europe': ['ITALY', 'SPAIN', 'PORTUGAL', 'GREECE', 'CROATIA', 'CYPRUS', 'SLOVENIA', 'GIBRALTAR', 'MALTA'],
+    'East Europe': ['CZECH REPUBLIC', 'POLAND',   'ROMANIA', 'HUNGARY', 'BULGARIA', 'SLOVAKIA', 'KOSOVO', 'UKRAINE'],
     'West Europe': ['AUSTRIA', 'GERMANY', 'SWITZERLAND', 'BELGIUM', 'LUXEMBOURG', 'FRANCE', 'NETHERLANDS'],
-    'East Asia': ['JAPAN', 'CHINA', 'HONG KONG'],
-    'Other': ['SOUTH AFRICA', 'SINGAPORE']
+    'East Asia': ['JAPAN', 'CHINA', 'HONG KONG', 'TAIWAN'],
+    'South Asia' : ['INDIA'],
+    'Middle east' : ['UNITED ARAB EMIRATES'],
+    'Oeania' : [ 'SINGAPORE', 'AUSTRALIA','NEW ZEALAND'],
+    'Other': ['SOUTH AFRICA', 'BERMUDA', 'GHANA', 'KAZAKHSTAN']
 }
 country_aff = {}
 for region in country_groups.keys():
     for c in country_groups[region]:
         country_aff [c] = region
 
-#data['TR Country'] = data['TR Country'].apply(lambda x : country_aff[x])
+data['TR Country'] = data['TR Country'].apply(lambda x : country_aff[x])
 
-threshold = 1 #percent
-data = remove_outliers(data, 'TR Country', threshold)
-data = remove_outliers(data, 'BvD sectors', threshold)
 
 binary = field + level
 continuous = lobbying+ financials
 nominal = country + sector
 
-X = data[continuous + binary + nominal]
-X, sector= one_hot_encod(X, sector)
-X, country= one_hot_encod(X, country)
+# Get columns with similar values
+sim_val_cols = get_similar_value_cols(data, percent=80)
+data = data.drop(columns = sim_val_cols)
+field = list(set(data.columns) & set(field) )
+level = list(set(data.columns) & set(level) )
+
+# Remove outliers
+threshold = 1 #percent
+data = remove_outliers(data, 'TR Country', threshold)
+data = remove_outliers(data, 'NACE', threshold)
+
+
+# Correlation between cenrtalities
+heatmap_corr (data,  dependent , annot = True)
+
+# log tranform skewed continous variables
+columns = list(get_skewed_columns(data[continuous +dependent]).index)
+data = transform_skewed(data, columns,'log')
+
+
+#X[level +country +financials + sector+ lobbying  ].to_csv(path + 'Programs/data_test_R.csv', index = False)
 
 Y = data[dependent]
 
-# heatmap_corr (X ,  continuous , annot = True)
-#xy_heatmap_corr(X, field, sector)
 
-
-columns = list(get_skewed_columns(X[continuous]).index)
-X = transform_skewed(X, columns,'log')
-#heatmap_corr (X ,  continuous , annot = True)
+#heatmap_corr (data,  dependent , annot = True)
+##
 
 
 
-
-X = X[field]
-X = sm.add_constant(X)
-
-Y = data[['Strength']]
-Y = Y.loc[X.index]
-results = sm.OLS(Y, X).fit()
+category_to_remove= {'Strength': {'Country' : 'East Europe', 'Sector' :'H - Transportation and storage'},
+                'Closeness': {'Country' : 'East Europe', 'Sector' :'S - Other service activities'},
+                'EV_max' : {'Country' :  'South Europe', 'Sector' : 'B - Mining and quarrying'},
+                'Hypercoreness': {'Country' : 'North America' , 'Sector' :'H - Transportation and storage'}}
 
 
+# Define variables for regression
+targets = ['Strength', 'EV_linear', 'Hypercoreness']
+variables = lobbying + financials + country + sector +level +field
+basetable = data[variables + targets]
+
+basetable, h_sector = one_hot_encod(basetable, sector, 'C - Manufacturing')
+basetable, h_country = one_hot_encod(basetable, country, 'West Europe')
+variables =  lobbying + financials + h_country + h_sector +field +level
+
+
+#xy_heatmap_corr(basetable, field, h_sector)
+
+
+# Normalize data
+basetable = pd.DataFrame(preprocessing.StandardScaler().fit_transform(basetable) , columns = basetable.columns, index = basetable.index)
+
+
+
+df_regressions = pd.DataFrame()
+for target in  ['Strength', 'EV_linear', 'Hypercoreness']:
+    target = 'Strength'
+    y = basetable[target]
+    X = basetable[variables]
+    results = sm.OLS(y ,X.assign(const = 1)).fit()
+
+    df = pd.DataFrame(results.params.round(3), columns = [target])
+    df.loc[(0.01<results.pvalues) & (results.pvalues <0.05)] = df.astype(str) + "*"
+    df.loc[(0.001<results.pvalues) & (results.pvalues <0.01)] = df.astype(str) + "**"
+    df.loc[results.pvalues <0.001] = df.astype(str) + "***"
+    df.loc[results.pvalues >0.05] = df.astype(str) +  ' '
+    df_regressions = pd.concat([df_regressions, df], axis = 1)
+
+#print(df_regressions.to_latex())
+print(df_regressions)
 print(results.summary())
+
+
+## Analysis of variance
+
+def pr_F(current_model , variables, tagret, basetable):
+    X = basetable[variables]
+    y = basetable[target]
+    model = sm.OLS(y, X.assign(cont = 1)).fit()
+    if model.df_model > current_model.df_model:
+        anova_results = sm.stats.anova_lm(current_model, model )
+    else:
+        anova_results = sm.stats.anova_lm( model , current_model)
+    return(anova_results['Pr(>F)'][1])
+
+
+def next_best (current_variables, candidate_variables, taget, basetable):
+    best_pr_F = 1
+    best_variable = None
+
+    X = basetable[current_variables]
+    y = basetable[target]
+    current_model = sm.OLS(y, X.assign(cont = 1)).fit()
+    for v in candidate_variables:
+        pr_F_v = pr_F( current_model , current_variables +[v], target, basetable)
+        if pr_F_v < best_pr_F and pr_F_v < 0.05:
+            best_pr_F = pr_F_v
+            best_variable = v
+    return best_variable
+
+def next_worst (current_variables, target, basetable):
+    worst_pr_F = 0
+    worst_variable = None
+
+    X = basetable[current_variables]
+    y = basetable[target]
+    current_model = sm.OLS(y, X.assign(cont = 1)).fit()
+    for v in current_variables:
+
+        variables = list( set(current_variables) - set([v]))
+        pr_F_v = pr_F( current_model , variables, target, basetable)
+        if pr_F_v > worst_pr_F and pr_F_v > 0.05 or np.isnan(pr_F_v) :
+            worst_pr_F = pr_F_v
+            worst_variable = v
+    return worst_variable
+
+def variables_to_add(current_variables, candidate_variables, target, basetable, threshold = 0.05):
+    variables  = []
+
+    X = basetable[current_variables]
+    y = basetable[target]
+    current_model = sm.OLS(y, X.assign(cont = 1)).fit()
+    for v in candidate_variables:
+        pr_F_v = pr_F( current_model , current_variables +[v], target, basetable)
+        if pr_F_v < threshold:
+            variables.append(v)
+    return variables
+
+def variables_to_remove(current_variables, target, basetable, threshold =0.05):
+
+    variables =[]
+    X = basetable[current_variables]
+    y = basetable[target]
+    current_model = sm.OLS(y, X.assign(cont = 1)).fit()
+    for v in current_variables:
+        red_variables = list( set(current_variables) - set([v]))
+        pr_F_v = pr_F( current_model , red_variables, target, basetable)
+        if pr_F_v > threshold or np.isnan(pr_F_v) :
+            variables.append(v)
+    return variables
+
+
+def forward_selection(candidate_variables, target, basetable):
+    current_variables = []
+    nb_itt_max = len(candidate_variables)
+    for i in range( nb_itt_max):
+        next_var = next_best(current_variables, candidate_variables, target, basetable)
+        if next_var is None:
+            break
+        current_variables = current_variables + [next_var]
+        candidate_variables.remove(next_var)
+    return(current_variables)
+
+def backword_selection(candidate_variables, target, basetable):
+    current_variables = lobbying + financials + h_country + h_sector
+    nb_itt_max = len(current_variables)
+
+    for i in range( nb_itt_max):
+        next_var = next_worst(current_variables, target, basetable)
+        if next_var is None:
+            break
+        current_variables.remove(next_var)
+    return(current_variables)
+
+
+    ##
+
+# forward selection
+target =  ['Strength']
+basetable = data[lobbying + financials + country + sector + target+level]
+basetable, h_sector = one_hot_encod(basetable, sector)
+basetable, h_country = one_hot_encod(basetable, country)
+
+candidate_variables = lobbying + financials + h_country + h_sector +level
+current_variables = forward_selection(candidate_variables, target, basetable)
+print(sm.OLS(basetable[target], basetable[current_variables].assign(const = 1)).fit().summary())
+
+#backword selection
+
+current_variables = backword_selection(candidate_variables, target, basetable)
+print(sm.OLS(basetable[target] , basetable[current_variables].assign(const = 1)).fit().summary())
+
+##
+
+def stepwise_selection(candidate_variables, target, basetable):
+    current_variables = []
+    nb_itt_max = len(candidate_variables)
+    while True:
+        c = 0
+        next_var = variables_to_add(current_variables, candidate_variables, target, basetable, threshold = teta_in)
+
+        if next_var ==[]:
+            c+=1
+        else :
+            current_variables = current_variables + next_var
+            for v in next_var:
+                candidate_variables.remove(v)
+        next_var  = variables_to_remove(current_variables, target, basetable, threshold = teta_out)
+        if next_var == []:
+            c+=1
+        else:
+            for v in next_var:
+                current_variables.remove(v)
+                candidate_variables.append(v)
+
+        if c == 2:
+            break
+    return(current_variables)
+
+
+
+# Define variables for regression
+targets = ['Strength', 'EV_linear', 'Hypercoreness']
+variables = lobbying + financials + country + sector +level
+basetable = data[variables + targets]
+
+basetable, h_sector = one_hot_encod(basetable, sector)
+basetable, h_country = one_hot_encod(basetable, country)
+candidate_variables = lobbying + financials + h_country + h_sector + level
+
+
+# Normalize data
+basetable = pd.DataFrame(preprocessing.StandardScaler().fit_transform(basetable) , columns = basetable.columns, index = basetable.index)
+
+#Stepwise selection
+teta_in = 0.09
+teta_out = 0.12
+target =  ['EV_linear']
+current_variables = stepwise_selection(candidate_variables, target, basetable)
+
+
+print(sm.OLS(basetable[target] , basetable[current_variables].assign(const = 1)).fit().summary(slim = False ))
+
+print(fr'$$\theta_i = {teta_in}, \theta_o ={teta_out}$$')
+#print(sm.OLS(basetable[target] , basetable[current_variables].assign(const = 1)).fit().summary(slim = True ).as_latex())
+
+##
+
+# Define variables for regression
+targets = ['Strength', 'EV_linear', 'Hypercoreness']
+variables = lobbying + financials + country + sector +level
+basetable = data[variables + targets]
+
+basetable, h_sector = one_hot_encod(basetable, sector)
+basetable, h_country = one_hot_encod(basetable, country)
+variables = lobbying + financials + h_country + h_sector + level
+
+
+# Normalize data
+basetable = pd.DataFrame(preprocessing.StandardScaler().fit_transform(basetable) , columns = basetable.columns, index = basetable.index)
+
+
+df_regressions = pd.DataFrame()
+df_regressions.index = lobbying + financials + h_country + h_sector  +level
+
+
+for target in  [['Strength'],['EV_linear'], ['Hypercoreness']]:
+    candidate_variables = lobbying + financials + h_country + h_sector  +level
+    y = basetable[target]
+    variables = stepwise_selection(candidate_variables, target, basetable)
+
+    X = basetable[variables]
+    results = sm.OLS(y, X.assign(const = 1)).fit()
+    print(results.rsquared)
+
+    df = pd.DataFrame(results.params.round(3), columns = target)
+    df.loc[(0.01<results.pvalues) & (results.pvalues <0.05)] = df.astype(str) + "*"
+    df.loc[(0.001<results.pvalues) & (results.pvalues <0.01)] = df.astype(str) + "**"
+    df.loc[results.pvalues <0.001] = df.astype(str) + "***"
+    df.loc[results.pvalues >0.05] = df.astype(str) +  ' '
+    df_regressions = pd.concat([df_regressions, df], axis = 1)
+
+df_regressions.columns = df_regressions.columns.str.replace('_', ' ')
+print(df_regressions)
+print(df_regressions.to_latex(na_rep = ''))
+
 
